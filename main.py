8d88ca69a67db6b1dd76a1c822de1c4033f3c5c2
Mary Chinonye Chukwu <chukwuchinonye063@gmail.com>
2025-03-19 11:32:20 +0000

    updated

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+logins = {}
+logins["david"] = {"email": "rdlnk@example.com", "password": "baldy1"}
+logins["katie"] = {"email": "rdlnk@example.com", "password": "kat12"}
+logins["yul"] = {"email": "plsgq@example.com", "password": "herm1"}
+
+@app.route("/login", methods=["POST"])
+def login():
+  form = request.form
+  isThere = False
+  details = {}
+  try:
+    details = logins[form["username"]]
+    isThere = True
+  except:
+    return "Username, email, or password incorrect"
+  if form["email"] == details["email"] and form["password"] == details["password"]:
+    return "You are logged in"
+  else:
+    return "Username, email, or password incorrect"
+
 
 
 @app.route('/')
 def index():
-  page = """<form method = "post" action="/process">
-    <p>Name: <input type="text" name="username" required> </p>
-    <p>Email: <input type="Email" name="email"> </p>
-    <p>Website: <input type="url" name="website"> </p>
-    <p>Age: <input type="number" name="age"> </p>
-    <input type="hidden" name="userID" value="232"> </p>
-
-    <p>
-      Fave Baldy: 
-      <select name="baldies">
-        <option>David</option>
-        <option>Jean Luc Picard</option>
-        <option>Yul Brynner</option>
+  page = """<form method="post" action="login">
+    <p>Username: <input type="text" name="username" required></p>
+    <p>Email: <input type="Email" name="email" required></p>
+    <p>Password: <input type="password" name="password" required></p>
+    <p>Gender:
+      <select name="gender">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
       </select>
     </p>
-
-    <button type="submit">Save Data</button>
+    <button type="submit">Login</button>
   </form>
     """
   return page
-  
+
 app.run(host='0.0.0.0', port=81)
